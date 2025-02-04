61b6b25cb95646977f0e41316cc70c41a1748dac
jeffrey-anaphero <jeffrey@anaphero.com>
2025-02-04 13:48:14 -0500

    Updates for testing

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import { sendCall } from './utils';
 
 async function main() {
-  // call sendCall three times with different jobId:
-  await sendCall('1', { scheduledAt: new Date() });
-  await sendCall('2', { scheduledAt: new Date() });
-  await sendCall('3', { scheduledAt: new Date() });
+  // Example of how to simulate a call for testing:
+  const metadatToPrint = {
+    scheduledAt: new Date(),
+  };
+  await sendCall('1', metadatToPrint);
 }
 
 main();
